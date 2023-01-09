58a80bf1d07e2589b99b687bac23cc73d817e741
nadifaifani <nadifaifani@gmail.com>
2023-01-09 20:34:51 +0700

    controller update

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -54,4 +54,65 @@ class PostController extends Controller
         //return response
         return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
+
+    /**
+     * show
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function show(Post $post)
+    {
+        //return single post as a resource
+        return new PostResource(true, 'Data Post Ditemukan!', $post);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $post
+     * @return void
+     */
+    public function update(Request $request, Post $post)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama_penjual'      => 'required',
+            'nama_toko'         => 'required',
+            'alamat'            => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        else {
+            $post->update([
+                'nama_penjual'      => $request->nama_penjual,
+                'nama_toko'         => $request->nama_toko,
+                'alamat'            => $request->alamat,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $post
+     * @return void
+     */
+    public function destroy(Post $post)
+    {
+        
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
+    }
 }
