3c51c925659c52f28e1d5c10bdbc45f5fc45ec13
nadifaifani <nadifaifani@gmail.com>
2023-01-09 20:15:36 +0700

    create add

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {    
@@ -21,5 +22,36 @@ class PostController extends Controller
 
         //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+    
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'nama_penjual'      => 'required',
+            'nama_toko'         => 'required',
+            'alamat'            => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $post = Post::create([
+            'nama_penjual'      => $request->nama_penjual,
+            'nama_toko'         => $request->nama_toko,
+            'alamat'            => $request->alamat,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
